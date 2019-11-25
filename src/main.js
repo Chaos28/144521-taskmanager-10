9dba9cbfa84dd1f04f277560c3552ed7c8c1da8a
Saire28 <huntergeass@gmail.com>
2019-11-25 18:42:40 +0300

    Update main.js

--- src/main.js
+++ src/main.js
@@ -3,8 +3,8 @@
 const TASK_COUNT = 3;
 
 const createSiteMenuTemplate = () => {
-  return (
-    `<section class="control__btn-wrap">
+  return (`
+    <section class="control__btn-wrap">
       <input
         type="radio"
         name="control"
@@ -468,7 +468,6 @@ render(siteMainElement, createBoardTemplate(), `beforeend`);
 
 const taskListElement = siteMainElement.querySelector(`.board__tasks`);
 render(taskListElement, createTaskEditorTemplate(), `beforeend`);
-
 new Array(TASK_COUNT)
   .fill(``)
   .forEach(
